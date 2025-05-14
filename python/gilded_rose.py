e5e05459351aeeb3c4c5d8d42ce509ee15157a52
raveendra battala <raveendra.battala83@gmail.com>
2025-05-14 07:26:13 +0530

    Add initial support for Conjured items in update_quality

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,12 +1,17 @@
-# -*- coding: utf-8 -*-
-
 class GildedRose(object):
-
     def __init__(self, items):
         self.items = items
 
     def update_quality(self):
         for item in self.items:
+            if "Conjured" in item.name:  # Temporary check for Conjured items
+                if item.quality > 0:
+                    item.quality = max(0, item.quality - 2 if item.sell_in > 0 else 4)
+                if item.name != "Sulfuras, Hand of Ragnaros":
+                    item.sell_in -= 1
+                continue
+
+            # Existing logic remains unchanged
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality > 0:
                     if item.name != "Sulfuras, Hand of Ragnaros":
@@ -34,13 +39,3 @@ class GildedRose(object):
                 else:
                     if item.quality < 50:
                         item.quality = item.quality + 1
-
-
-class Item:
-    def __init__(self, name, sell_in, quality):
-        self.name = name
-        self.sell_in = sell_in
-        self.quality = quality
-
-    def __repr__(self):
-        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
