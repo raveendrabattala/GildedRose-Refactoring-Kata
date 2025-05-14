b1c03160c53bf168b47ece2bd56ff7999938a826
raveendra battala <raveendra.battala83@gmail.com>
2025-05-14 07:31:36 +0530

    Add tests for edge cases to improve coverage

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -27,3 +27,31 @@ def test_conjured_item_quality_never_above_50():
     gilded_rose = GildedRose(items)
     gilded_rose.update_quality()
     assert items[0].quality == 48  # Decreases by 2
+
+# Add to existing tests
+def test_aged_brie_quality_increases_after_sell_in():
+    items = [Item("Aged Brie", 0, 48)]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    assert items[0].sell_in == -1
+    assert items[0].quality == 49
+
+def test_backstage_passes_quality_zero_after_concert():
+    items = [Item("Backstage passes to a TAFKAL80ETC concert", 0, 20)]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    assert items[0].sell_in == -1
+    assert items[0].quality == 0
+
+def test_sulfuras_no_change():
+    items = [Item("Sulfuras, Hand of Ragnaros", 5, 80)]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    assert items[0].sell_in == 5
+    assert items[0].quality == 80
+
+def test_normal_item_quality_never_above_50():
+    items = [Item("Elixir of the Mongoose", 5, 50)]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    assert items[0].quality == 49
