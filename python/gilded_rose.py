a94a4160b3a605109b0b12fa83d032b3247ce87e
raveendra battala <raveendra.battala83@gmail.com>
2025-05-14 07:29:36 +0530

    Refactor to Strategy pattern with item-specific updaters

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -8,49 +8,64 @@ class Item:
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
-# Constants for item names
+# Constants
 AGED_BRIE = "Aged Brie"
 BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
 SULFURAS = "Sulfuras, Hand of Ragnaros"
 CONJURED = "Conjured"
 
-class GildedRose(object):
+class ItemUpdater:
+    def update(self, item):
+        pass
+
+    def _constrain_quality(self, quality):
+        return max(0, min(50, quality))
+
+class NormalItemUpdater(ItemUpdater):
+    def update(self, item):
+        item.sell_in -= 1
+        item.quality = self._constrain_quality(item.quality - (1 if item.sell_in >= 0 else 2))
+
+class AgedBrieUpdater(ItemUpdater):
+    def update(self, item):
+        item.sell_in -= 1
+        item.quality = self._constrain_quality(item.quality + 1)
+
+class BackstagePassUpdater(ItemUpdater):
+    def update(self, item):
+        item.sell_in -= 1
+        increase = 1
+        if item.sell_in < 10:
+            increase += 1
+        if item.sell_in < 5:
+            increase += 1
+        if item.sell_in < 0:
+            item.quality = 0
+        else:
+            item.quality = self._constrain_quality(item.quality + increase)
+
+class SulfurasUpdater(ItemUpdater):
+    def update(self, item):
+        pass  # Sulfuras doesn't change
+
+class ConjuredItemUpdater(ItemUpdater):
+    def update(self, item):
+        item.sell_in -= 1
+        item.quality = self._constrain_quality(item.quality - (2 if item.sell_in >= 0 else 4))
+
+class GildedRose:
     def __init__(self, items):
         self.items = items
+        self.updaters = {
+            AGED_BRIE: AgedBrieUpdater(),
+            BACKSTAGE_PASSES: BackstagePassUpdater(),
+            SULFURAS: SulfurasUpdater(),
+        }
 
     def update_quality(self):
         for item in self.items:
             if CONJURED in item.name:
-                if item.quality > 0:
-                    item.quality = max(0, item.quality - 2 if item.sell_in > 0 else 4)
-                if item.name != SULFURAS:
-                    item.sell_in -= 1
-                continue
-
-            if item.name != AGED_BRIE and item.name != BACKSTAGE_PASSES:
-                if item.quality > 0:
-                    if item.name != SULFURAS:
-                        item.quality = item.quality - 1
+                updater = ConjuredItemUpdater()
             else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == BACKSTAGE_PASSES:
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != SULFURAS:
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != AGED_BRIE:
-                    if item.name != BACKSTAGE_PASSES:
-                        if item.quality > 0:
-                            if item.name != SULFURAS:
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                updater = self.updaters.get(item.name, NormalItemUpdater())
+            updater.update(item)
